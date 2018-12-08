f56f9d992612bb6f6e511ef74675e32501abddbe
Feng <37281270+Feng2468339827@users.noreply.github.com>
2018-12-08 22:19:49 +0800

    Add files via upload

--- form/form.php
+++ form/form.php
@@ -1,6 +1,5 @@
 <?php
-
-header('Content-Type: text/plain; charset=utf-8');
+	header('Content-Type: text/plain; charset=utf-8');
     var_dump($_FILES);
 	$imgname = $_FILES['myfile']['name'];
     $tmp = $_FILES['myfile']['tmp_name'];
