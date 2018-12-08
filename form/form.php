365a719b119da17b24d9c29646ffedb5d96f178e
Feng <37281270+Feng2468339827@users.noreply.github.com>
2018-12-08 20:44:03 +0800

    Add files via upload
    这是一个用form表单上传图片到后台的小demo，附上两种图片预览方式

---
+++ form/form.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Content-Type: text/plain; charset=utf-8');
+    var_dump($_FILES);
+	$imgname = $_FILES['myfile']['name'];
+    $tmp = $_FILES['myfile']['tmp_name'];
+    $filepath = './image';
+    if(move_uploaded_file($tmp,"$filepath/$imgname")){
+        echo "上传成功";
+    }else{
+        echo "上传失败";
+    }
+?>
